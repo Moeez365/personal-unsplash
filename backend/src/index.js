8d011614a2f8753c6235ec6cd4ae9254fa0511b1
Muhammad Moeez Nadeem <moeeznadeem58@gmail.com>
2026-03-03 19:11:46 +0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ import { ApiError } from "./utils/errors.utils.js";
 
 const app = express();
 app.use(express.json());
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(router);
 
 mongoDB();
