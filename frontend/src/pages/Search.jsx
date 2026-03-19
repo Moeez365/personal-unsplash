d533bc7d37989d6320674660f7695ed031f948c3
moeez365 <moeeznadeem58@gmail.com>
2026-03-19 21:10:28 +0500

    fix the small bugs on search page

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -10,38 +10,47 @@ const Search = () => {
   const [responseData, setResponseData] = useState([]);
   const [page, setPage] = useState(1);
 
-  const searchImages = async (search) => {
+  const searchImages = async (search, page) => {
     try {
-      if (page==1) {
+      if (!page) {
+        // When this function is called from the toNavigate function, no page parameter is passed,
+        // therefore the default page is 1.
+        // Before fetching the API, we clear the previous data.
         setResponseData([]);
+        page = 1;
       }
       const res = await axios.get(
         `https://api.unsplash.com/search/photos?client_id=${import.meta.env.VITE_CLIENT_ID}&page=${page}&query=${search.split("-").join(" ")}`,
       );
-      setResponseData(prev => [...prev, ...res.data.results]);
+
+      const result = res.data.results || [];
+      const totalPages = res.data.total_pages;
+
+      if (page <= totalPages) {
+        setResponseData((prev) => [...prev, ...result]);
+      }
     } catch (error) {
       console.error(error);
     }
   };
 
-  console.log(responseData);
-
   const toNavigate = (search) => {
     navigate(`/search?name=${search}`);
     searchImages(search);
-  }
+  };
 
   const handleSearch = (e) => {
     if (e.key == "Enter" && search) {
-      toNavigate(search)
+      toNavigate(search);
     }
   };
 
   useEffect(() => {
-    searchImages(search);
+    searchImages(search, page);
   }, [page]);
 
   useEffect(() => {
+    window.scrollTo({top:0, behavior:'smooth'})
     const handleScroll = () => {
       const pageSize = document.documentElement.scrollHeight;
       const scrollSize =
@@ -56,14 +65,14 @@ const Search = () => {
 
   return (
     <div className="mt-15 relative">
-      <div className="sticky top-15">
-        <div className="bg-[url(/gradiend-bg.svg)] bg-cover bg-center h-16" />
-        <div className="-mt-6 full flex justify-center">
+      <div className="bg-[url(/gradiend-bg.svg)] bg-cover bg-center h-16" />
+      <div className="sticky top-19">
+        <div className="full flex justify-center">
           <div className="flex bg-(--white-color) items-center justify-center h-15 w-full mx-4 md:w-xl border border-(--primary-color) rounded-lg shadow-xl shadow-[#00000017]">
             <input
               onChange={(e) => setSearch(e.target.value)}
               onKeyDown={handleSearch}
-              defaultValue={search.split('-').join(" ")}
+              defaultValue={search.split("-").join(" ")}
               type="text"
               placeholder="Enter any keyword"
               className="py-4 px-4 h-full w-full rounded-l-lg outline-0"
